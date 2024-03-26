b09737d553952a704caf063af9d06eaca4b1c40b
supriyagk <gksupriya09@gmail.com>
2024-03-26 09:01:36 +0530

    ver 1.1

--- Fruit sell management system.c
+++ Fruit sell management system.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-<<<<<<< HEAD
 int main()
 {
     int MenuOption ; // initializing Variable for selecting option;
@@ -49,35 +48,5 @@ int main()
         }
     } while (MenuOption < 1 || MenuOption > 4); // Loop until a valid option is entered
 
-=======
-
-int main() {
-
-    /*const: This keyword indicates that the data being pointed to is constant, meaning it cannot be modified.
-
-    char *menu[]: This declares an array of pointers to characters.
-    Each element of the array menu is a pointer to a string (a sequence of characters).
-    Since it's an array of pointers, it's commonly used to store an array of strings*/
-    const char *menu[] = {"order Mango", "order Apple", "order Butterfruit", "order Strawberry", "order Litchi"};
-    int MenuOption;
-
-    /* here enter your option here*/
-    printf(" 1)Mango\n 2)Apple\n 3)Butterfruit\n 4)Strawberry\n 5)Litchi\n Enter your option:");
-    scanf("%d", &MenuOption);
-
-    /* if you select option 1 to 5 it select options anyone which option you sleected */
-    if (MenuOption >= 1 && MenuOption <= 5) {
-
-    //then it select the option and ordered successfully indicates;
-    printf("%s\n", menu[MenuOption - 1]);
-    printf("Ordered Successfully\n");
-
-    //else it shows invalid option, and it shows enter a correct option;
-    } else {
-    printf("Invalid option\n");
-    printf("Please enter a correct option\n");
-    }
-    //return statement shows function executed successfully;
->>>>>>> b8652b058c3408abe0e8f7208d7044ab9245875d
     return 0;
 }
